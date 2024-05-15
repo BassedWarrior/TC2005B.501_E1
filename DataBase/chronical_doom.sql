710f013646b06624251a361db2e6fd82c88fd5fc
Eduardo Porto Morales <165714406+Porto1090@users.noreply.github.com>
2024-05-14 18:56:04 -0600

    Overall checking of table structures in 'chronical_doom.sql'

---
+++ DataBase/chronical_doom.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE IF NOT EXISTS chronical_doom;
+USE chronical_doom;
+
+CREATE TABLE player (
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    creationDate DATE NOT NULL,
+    highscore_deck INT UNSIGNED NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE card (
+    cardID TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    cost TINYINT UNSIGNED NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    description TINYTEXT NOT NULL,
+    category ENUM('unit', 'paradox') NOT NULL,
+    health TINYINT UNSIGNED,
+    attack TINYINT UNSIGNED,
+    PRIMARY KEY (cardID) AUTOINCREMENT,
+);
+
+CREATE TABLE deck (
+    deckID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    creationDate DATE NOT NULL,
+    owner VARCHAR(20) NOT NULL,
+    PRIMARY KEY (deckID),
+    FOREIGN KEY (owner) REFERENCES player(username)
+);
+
+CREATE TABLE match (
+    matchID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL,
+    deck INT UNSIGNED NOT NULL,
+    datePlayed DATE NOT NULL,
+    timePlayed TIME NOT NULL,
+    score SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (matchID),
+    FOREIGN KEY (username) REFERENCES player(username),
+    FOREIGN KEY (deck) REFERENCES deck(deckID)
+);
+
+CREATE TABLE game (
+    gameID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL,
+    score SMALLINT UNSIGNED NOT NULL,
+    gameRound INT UNSIGNED NOT NULL,
+    kronos TINYINT UNSIGNED NOT NULL,
+    deckCards TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (gameID),
+    FOREIGN KEY (username) REFERENCES player(username),
+    FOREIGN KEY (gameRound) REFERENCES gameRound(roundID)
+);
+
+CREATE TABLE deckCard (
+    deckID INT UNSIGNED NOT NULL,
+    cardID TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (deckID, cardID),
+    FOREIGN KEY (deckID) REFERENCES deck(deckID),
+    FOREIGN KEY (cardID) REFERENCES card(cardID)
+);
+
+CREATE TABLE gameRound (
+    roundID TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    cardID TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (roundID, cardID),
+    FOREIGN KEY (cardID) REFERENCES card(cardID)
+);
+
