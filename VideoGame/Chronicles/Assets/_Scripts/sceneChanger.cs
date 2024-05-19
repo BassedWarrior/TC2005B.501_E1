eed1639975ed11b16488c9376cc1b78338f741fc
julianRamirez <A01027743@tec.mx>
2024-05-19 04:52:46 -0600

    added the main menu scene

---
+++ VideoGame/Chronicles/Assets/_Scripts/sceneChanger.cs
@@ -0,0 +1,17 @@
+/* 
+by: julian ramirez A01027742.
+18 de mayo 
+
+This script help us to change the scene from to main menu, 
+to access the game.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneChanger : MonoBehaviour
+{
+    public static void GoTo(string sceneName){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);}
+}
