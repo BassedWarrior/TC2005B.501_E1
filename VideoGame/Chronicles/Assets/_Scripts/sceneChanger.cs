48517a02d77e23067e3729e562b7c21690a7a69c
Eduardo Porto <A01027893@tec.mx>
2024-05-26 09:37:36 -0600

    Ready to be reviewed

--- VideoGame/Chronicles/Assets/_Scripts/sceneChanger.cs
+++ VideoGame/Chronicles/Assets/_Scripts/sceneChanger.cs
@@ -1,40 +1,49 @@
-/* 
-by: julian ramirez A01027742.
+/*
+by: Julian Ramirez A01027742.
 18 de mayo 
 
-This script help us to change the scene from to main menu, to access the game.
+This script helps us to change the scene from the main menu, to access the game.
 */
 
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     [SerializeField] private GameObject welcomeMessage;
+    [SerializeField] private GameObject deckMessage;
+
     public void ChangeToMenuScene()
     {
         welcomeMessage.SetActive(true);
-        StartCoroutine(ExecuteAfterTime(3f));
-        GoToScene("GameScene");
+        StartCoroutine(ExecuteAfterTime(2f, () => {
+            GoToScene("CollectionScene");
+        }));
     }
 
     public void LetsPlay()
     {
         GameManager gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
-        if(gameManager.playersDeck.Count > 6)
+        if (gameManager.playersDeck.Count > 6)
         {
             GoToScene("GameScene");
         }
+        else
+        {
+            deckMessage.SetActive(true);
+            StartCoroutine(ExecuteAfterTime(2f, () => deckMessage.SetActive(false)));
+        }
     }
 
-    private IEnumerator ExecuteAfterTime(float seconds)
+    private IEnumerator ExecuteAfterTime(float seconds, System.Action afterWaitAction)
     {
         yield return new WaitForSeconds(seconds);
+        afterWaitAction?.Invoke();
     }
 
     public void GoToScene(string sceneName)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName);
     }
 }
