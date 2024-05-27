cbd9e6f2ed2cd545c5bb252649a6e56682a21323
Eduardo Porto <A01027893@tec.mx>
2024-05-27 14:20:21 -0600

    Correct card Distribution inGame

--- VideoGame/Chronicles/Assets/_Scripts/APIConnection.cs
+++ VideoGame/Chronicles/Assets/_Scripts/APIConnection.cs
@@ -5,31 +5,15 @@ using UnityEngine.Networking;
 
 public class APIConnection : MonoBehaviour
 {
-    [System.Serializable]
-    public class CardData
-    {
-        public int ID;
-        public string name;
-        public string description;
-        public string artworkPath;
-        public int energyCost;
-        public int health;
-        public int attack;
-    }
+    private GameManager gameManager;
     [SerializeField] string url;
     [SerializeField] string getEndpoint;
     [SerializeField] string getCardsEndpoint;
     [SerializeField] string getUnitCardsEndpoint;
     [SerializeField] string getParadoxCardsEndpoint;
 
-    // TODO: Add game controller
-    // controller;
-
-    // Start is called before the first frame update
     void Start()
     {
-        // TODO: Add game controller
-        // controller = ;
         GetCards();
     }
 
@@ -62,32 +46,16 @@ public class APIConnection : MonoBehaviour
                 string result = www.downloadHandler.text;
                 Debug.Log("The response was: " + result);
                 CardData[] cardDataArray = JsonUtility.FromJson<CardDataArrayWrapper>(result).cards;
+                gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
                 foreach(CardData cardData in cardDataArray) {
-                    UpdateScriptableObject(cardData);
+                    UpdateCards(cardData);
                 }
-
-                // Start the process to create the simon buttons
-                // TODO: Add controller
-                // controller.apiData = result;
-                // controller.PrepareButtons();
             }
         }
     }
-    void UpdateScriptableObject(CardData cardData)
+    void UpdateCards(CardData cardData)
     {
-        CardCreator card = ScriptableObject.CreateInstance<CardCreator>();
-        card.ID = cardData.ID;
-        card.name = cardData.name;
-        card.description = cardData.description;
-        card.energyCost = cardData.energyCost;
-        card.health = cardData.health;
-        card.attack = cardData.attack;
-        card.artwork = Resources.Load<Sprite>("alien");
-        
-        string relativeOutputPath = "Assets/PreFabs/ScriptableObjects";
-        string outputFilePath = relativeOutputPath + "/" + card.name + ".asset";
-
-        UnityEditor.AssetDatabase.CreateAsset(card, outputFilePath);
+        gameManager.cards.Add(cardData);
     }
 
     [System.Serializable]
