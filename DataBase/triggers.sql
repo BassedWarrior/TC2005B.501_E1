f07d93e9f08e544e3d0088b9534e602661c94dad
Eduardo Porto Morales <165714406+Porto1090@users.noreply.github.com>
2024-05-31 11:54:52 -0600

    modifying deck quantity to just one

--- DataBase/triggers.sql
+++ DataBase/triggers.sql
@@ -7,13 +7,9 @@ AFTER INSERT ON player
 FOR EACH ROW
 BEGIN
     DECLARE deck_name VARCHAR(30);
-    DECLARE i INT DEFAULT 1;
 
-    WHILE i <= 3 DO
-        SET deck_name = CONCAT('DECK ', i, ": ", NEW.username);
-        INSERT INTO deck (name, creationDate, owner) VALUES (deck_name, CURDATE(), NEW.username);
-        SET i = i + 1;
-    END WHILE;
+    SET deck_name = CONCAT('DECK: ', NEW.username);
+    INSERT INTO deck (name, creationDate, owner) VALUES (deck_name, CURDATE(), NEW.username);
 END //
 
 DELIMITER ;
